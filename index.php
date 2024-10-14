9072c53a68f61b64bc5e7b2fb00b39c6bf61799b
Kirill Shustaev <kirill.shustaev@yandex.ru>
2024-10-14 17:54:29 +0300

    desk

--- index.php
+++ index.php
@@ -39,6 +39,7 @@
                     </span>
                     <span class="hero__titleFriday">
                         пятница
+                        <span class="hero__sale"></span>
                     </span>
                 </h1>
                 <span class="hero__star">
@@ -48,10 +49,41 @@
                         ./img/main_star_1440_2x.png 2x,
                         ./img/main_star_1440_3x.png 3x
                         "
-                        alt="Изображение товара">
+                        alt="Изображение">
                     </picture>
                 </span>
+                <div class="hero__btnBlock flex">
+                    <p class="hero__descr">
+                        Воспользуйтесь выгодными предложениями от Roistat в ноябре*
+                    </p>
+                    <button class="hero__btn flex">
+                        Получить выгоду
+                        <svg width="24" height="18" viewBox="0 0 24 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M4 8L22 8" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                        <path d="M16 1L23 8L16 15" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                        </svg>
+                    </button>
+                </div>
+                <div class="hero__lineBlock">
+                    <div class="hero__line hero__line_top flex">
+                        <span class="hero__lineStar hero__lineStar_top"></span>
+                        <span class="hero__lineTxt hero__lineTxt_top"></span>
+                        <span class="hero__lineStar hero__lineStar_top"></span>
+                        <span class="hero__lineTxt hero__lineTxt_top"></span>
+                        <span class="hero__lineStar hero__lineStar_top"></span>
+                        <span class="hero__lineTxt hero__lineTxt_top"></span>
+                    </div>
+                    <div class="hero__line hero__line_bottom flex">
+                        <span class="hero__lineStar hero__lineStar_bottom"></span>
+                        <span class="hero__lineTxt hero__lineTxt_bottom"></span>
+                        <span class="hero__lineStar hero__lineStar_bottom"></span>
+                        <span class="hero__lineTxt hero__lineTxt_bottom"></span>
+                        <span class="hero__lineStar hero__lineStar_bottom"></span>
+                        <span class="hero__lineTxt hero__lineTxt_bottom"></span>
+                    </div>
+                </div>
             </div>
+
         </section>
     </main>
 </body>
